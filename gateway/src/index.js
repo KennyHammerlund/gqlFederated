9e85512c58a702254f6ee24efc97268c5d0b0e49
Kenny Hammerlund <kenny.hammerlund@gmail.com>
2021-10-11 17:32:57 -0500

    clean up

--- gateway/src/index.js
+++ gateway/src/index.js
@@ -1,28 +1,22 @@
 const { ApolloServer } = require("apollo-server");
 const { ApolloGateway } = require("@apollo/gateway");
-const { readFileSync } = require("fs");
 const {
   ApolloServerPluginLandingPageGraphQLPlayground,
 } = require("apollo-server-core");
 
-const supergraphSdl = readFileSync("./supergraph.graphql").toString();
 // Initialize an ApolloGateway instance and pass it
-// the supergraph schema
+// a list of our services.
 const gateway = new ApolloGateway({
   serviceList: [
     { name: "recommendations", url: "http://localhost:3366" },
-    { name: "products", url: "http://localhost:3355/graphql" },
+    { name: "products", url: "http://localhost:3355" },
   ],
   debug: true,
 });
 
-// Pass the ApolloGateway to the ApolloServer constructor
 const server = new ApolloServer({
   gateway,
-  plugins: [
-    ApolloServerPluginLandingPageGraphQLPlayground(),
-    // winstonLogger(logger),
-  ],
+  plugins: [ApolloServerPluginLandingPageGraphQLPlayground()], // ditch the annoying apollo landing page
 });
 
 server.listen().then(({ url }) => {
